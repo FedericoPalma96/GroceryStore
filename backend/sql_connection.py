d9d49e9758988ecc7f531831bebcf31ee799964d
FedericoPalma96 <federicos.palma@gmail.com>
2024-05-14 12:16:34 -0300

    commit final

---
+++ backend/sql_connection.py
@@ -0,0 +1,8 @@
+import mysql.connector
+
+__cnx = None
+def get_sql_connection():
+    global __cnx
+    if __cnx is None:
+        __cnx = mysql.connector.connect(user='root', password='123as',host='127.0.0.1',database='store')
+    return __cnx
